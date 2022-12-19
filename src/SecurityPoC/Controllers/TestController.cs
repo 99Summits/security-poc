419002fb68aeda3ebf206b3ebe941e3930db1601
Kevin Jordan <kevin@99summits.com>
2022-12-19 10:50:13 -0600

    Add vulnerable dependency.

---
+++ src/SecurityPoC/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecurityPoC.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("/test")]
+        public IActionResult Index([FromQuery] string jsonString)
+        {
+            var myObject = Newtonsoft.Json.JsonConvert.DeserializeObject<MyObject>(jsonString);
+            return Ok(myObject.Value);
+        }
+
+        public class MyObject 
+        {
+            public string? Value { get; set; }
+        }
+    }
+}
